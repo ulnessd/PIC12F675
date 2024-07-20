192164c6b17b98bac29751d259e7d4ad632fe0a0
Darin J. Ulness <120402820+ulnessd@users.noreply.github.com>
2024-07-19 19:24:25 -0500

    Create interupttest.s
    not working yet

---
+++ interupttest.s
@@ -0,0 +1,93 @@
+; PIC12F675 Configuration Bit Settings
+;Simple interupt program
+    
+; Assembly source line config statements    
+
+; CONFIG
+  CONFIG  FOSC = INTRCIO        ; Oscillator Selection bits
+  CONFIG  WDTE = OFF            ; Watchdog Timer Enable bit
+  CONFIG  PWRTE = OFF           ; Power-Up Timer Enable bit
+  CONFIG  MCLRE = OFF           ; GP3/MCLR pin function select
+  CONFIG  BOREN = ON            ; Brown-out Detect Enable bit
+  CONFIG  CP = OFF              ; Code Protection bit
+  CONFIG  CPD = OFF             ; Data Code Protection bit
+
+#include <xc.inc>
+
+PSECT resetVect, class=CODE, delta=2, abs, reloc=0x0000
+resetVect:
+    PAGESEL main
+    goto main
+
+PSECT intVect, class=CODE, delta=2, abs, reloc=0x0004
+intVect:
+    PAGESEL ISR
+    goto ISR  ; Interrupt vector    
+
+main:
+    ; initialization of GPIO
+    bcf STATUS, 5   ;this shifts us to bank 0
+    clrf GPIO	    ;sets the GPIO pins to zero
+    movlw 0b00000111	;this selects the comparator setting. see page 37 for the settings
+    movwf CMCON	    ;this writes the above setting to the comparator. 
+    bsf STATUS, 5   ;this shifts us to bank 1
+    movlw 0b00000000	;this selects ansel setting. see page 44 for the settings
+    movwf ANSEL	    ;this writes the above setting to the ansel 
+    movlw 0b00001100 ; first four zeros are irrelevent. 
+		     ; last five bits: GP5, GP4, GP3, GP2, GP1, GP0
+		     ; 1 will set to input, 0 will set to output
+		     ; GP3 is input only.
+    movwf TRISIO
+
+
+    ; Enable interrupt on GP2 change
+    bsf INTCON, 4    ; Enable external interrupt on GP2. See page 13
+    bsf OPTION_REG, 6 ; Interrupt on rising edge (clear for falling edge)
+    bsf INTCON, 7     ; Enable global interrupts. See page 13
+
+    bcf STATUS, 5   ;this shifts us to bank 0
+    
+    
+mainloop:
+    bsf GP0
+    movlw 25
+    call delay
+    bcf GP0
+    movlw 25
+    call delay
+    goto mainloop
+
+    
+delay: ; this is a three-layer nested loop. wouldn't need 3 but it is what I had 
+	; from other programs
+    movwf 0xB2
+out_out_loop:
+    movwf 0xB1
+outer_loop:
+    movwf 0xB0
+delay_loop:
+    decfsz 0xB0, 1
+    goto delay_loop
+    decfsz 0xB1, 1
+    goto outer_loop
+    decfsz 0xB2, 1
+    goto out_out_loop
+    retlw 0 ; the return sets the working register to zero. 
+    nop 
+    
+    
+PSECT isr_code, class=CODE, delta=2
+ISR:
+testloop:
+    bsf GP5
+    goto testloop
+    bsf STATUS, 5
+    btfss INTCON, 1 ; Check if it?s the external interrupt
+    retfie; If not, return from interrupt
+    
+    bcf INTCON, 1   ; Clear interrupt flag
+    bsf GP5
+    ;movlw 45
+    ;call delay
+    bcf STATUS, 5
+    retfie
